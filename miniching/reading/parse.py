257029504fc88003c2096bcd50b0ba3eb9b6905c
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-04-28 22:12:39 +0200

    automated quick commit

--- miniching/reading/parse.py
+++ miniching/reading/parse.py
@@ -1,10 +1,15 @@
+import os
 from textwrap import wrap
+from miniching import config
 from miniching.config import WIDTH, LINE_BREAK, SECTION_BREAK, INDENT
 from miniching.reading.models import Reading
+
+
+_DEFAULT_HISTORY_DIR = os.environ['HOME']
 _parsed_reading = []
+    
 
-
-def get_history_record(reading: Reading, width):
+def write_history(reading: Reading, width, path=config.HISTORY_PATH):
     record = []
 
     record.append(f"{str(reading.timestamp).center(WIDTH)}\n")
@@ -13,5 +18,12 @@ def get_history_record(reading: Reading, width):
     else:
         record.append(reading.query.center(WIDTH))
     
+    if not path or path == 'default': 
+        path = os.path.join(_DEFAULT_HISTORY_DIR, "iching-history.txt")
+    elif not os.path.isfile(path):
+        path = os.path.join(path, "iching-history.txt")
+
+    with open(path, "a+") as f:
+        f.write(record)
 
 
--- miniching/serialization.py
+++ miniching/serialization.py
@@ -1,29 +1,14 @@
 import os
 import yaml
 
-from miniching import config
 
-
-def read_yaml(path) -> dict:
+def _read_yaml(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)
 
-
-def write_history(parsed_reading: str):
-    path_dir = config.HISTORY_DIR
-    if path_dir == "default" or not path_dir:
-        path = os.path.join(_DEFAULT_HISTORY_DIR, "iching-history.txt")
-    else:
-        path = os.path.join(path_dir, "iching-history.txt")
-
-    with open(path, "a+") as f:
-        f.write(parsed_reading)
-
-
 _ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 _RESOURCES_DIR = os.path.join(_ROOT_DIR, "resources")
-_DEFAULT_HISTORY_DIR = os.environ['HOME']
 
-REFERENCE = read_yaml(os.path.join(_RESOURCES_DIR, "iching_reference.yaml"))
-BINARY_TO_DECIMAL = read_yaml(os.path.join(_RESOURCES_DIR, "binary_to_decimal.yaml"))
+REFERENCE = _read_yaml(os.path.join(_RESOURCES_DIR, "iching_reference.yaml"))
+BINARY_TO_DECIMAL = _read_yaml(os.path.join(_RESOURCES_DIR, "binary_to_decimal.yaml"))
 DECIMAL_TO_BINARY = {value: key for key, value in BINARY_TO_DECIMAL.items()}
